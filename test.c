8ac17d73ee65b3dab6f6cd89e9befeea17f9b99b
runweige <runweige@gmail.com>
2019-07-08 20:42:53 +0800

    crazy_cow

---
+++ test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void)
+{
+	int count = 1;
+	printf("%d%d%d",++count,++count,++count);
+	return 0;
+}
---
+++ test_c_exam.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+	int a = 8;
+	printf("%d",a+=a-=a*a);
+	return 0;
+}
+
