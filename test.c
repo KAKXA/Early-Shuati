e88f041c1df5c90d3cbf5cbcdac8220b55dac1c4
ns <notanemail@notanemail.notanemail>
2019-07-15 19:34:24 +0800

    2019.7.15 reversedNum.c(leetcode 9)

--- test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	int count = 1;
-	printf("%d%d%d",++count,++count,++count);
-	return 0;
-}
--- test_c_exam.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	int a = 8;
-	printf("%d",a+=a-=a*a);
-	return 0;
-}
-
