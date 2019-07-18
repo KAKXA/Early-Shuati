6754ab7884121c7b020c6be9d92f40785b9cad63
ns <notanemail@notanemail.notanemail>
2019-07-18 17:19:21 +0800

    2019.7.18

---
+++ Leetcode/Java/_3_maxSubString.java
@@ -0,0 +1,31 @@
+class _3_maxSubString
+{
+	public  static int lengthOfLongestSubstring(String s) 
+	{
+		for( int i = 0 ; i < s.length() ; i++ ) 
+		{
+			s.charAt(i);
+		}
+		return 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args)
+	{
+		String a = "123124";
+		lengthOfLongestSubstring(a);
+	}
+}
+
+
+			
