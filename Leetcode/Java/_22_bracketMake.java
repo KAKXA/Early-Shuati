7248543209a359e8ff50d97327cdb3ebf0c704da
runweige <runweige@gmail.com>
2019-07-30 15:45:00 +0800

    Leetcode 22的补充解法

--- Leetcode/Java/_22_bracketMake.java
+++ Leetcode/Java/_22_bracketMake.java
@@ -44,3 +44,45 @@ class _22_bracketMake{
         return temp;
     }
 }
+//做任何算法题之前，判断该题的“核心控制元素”是什么
+
+//下面是正儿八经的回溯法（递归）
+//原理是一个合理的括号字符串满足(这就是核心控制元素)
+//1，从左往右填充时左括号始终数始终小于等于总位数/2
+//2，		   右括号数始终小于等于左括号数
+//3，		   最终总长度为预期总长度，将其填入List
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+}
+
+//以下是与该题在Python区同思路的做法，Java实现没有使用动态规划所以略逊一筹，原理在22_bracketMake.py
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
+    }
+}
