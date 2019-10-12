89358669f12f4eef4e8bdbe34cd19def10dd64c7
zgk <runweige@gmail.com>
2019-10-12 21:19:55 +0800

    modify 2001 2005 2049

--- hd/2049.cpp
+++ hd/2049.cpp
@@ -1,25 +1,44 @@
 #include<cstdio>
-int t(int n,int m){
-	int r = 1;
+__int64 t(__int64 n,__int64 m){
+	__int64 r = 1;
 	for(int i = n; i > n - m; i--){
 		r *= i;
 	}
 	return r;
+}
+__int64 c(__int64  n,__int64 m){
+	if(n < m){
+		return 0;
+	}
+	if(m > n/2){
+		m = n - m;
+	}
+	return  t(n,m)/t(m,m);
+}
+
+void f(__int64 * ar, int n){
+	if(n > 2){
+		f(ar,n-1);
+		ar[n] = 0;
+		for(int i = 2; i < n; i++){
+			ar[n] += ar[i]*c(n,i);
+		}
+		ar[n] = t(n,n) - ar[n] - 1;
+	}
+	else if(n == 2){
+		ar[2] = 1;
+	}
 }
 int main(){
 	int num;
 	char ch;
 	scanf("%d%c",&num,&ch);
 	for(int i = 0; i < num; i++){
-		int temp;
 		int n,m;
 		scanf("%d%c%d%c",&n,&ch,&m,&ch);
-		if(m > n/2){
-			temp = n - m;
-		}
-		else{
-			temp = m;
-		}
-		printf("%d\n",t(n,temp)/t(temp,temp)*(t(m,m)-1));
+		__int64 * ar = new __int64[m+1];
+		f(ar,m);
+		printf("%I64ld\n",c(n,m)*ar[m]);
+		delete[] ar;
 	}
 }
---
+++ hd/inspiration.md
@@ -0,0 +1,7 @@
+# INSPIRATION
+
+## 2049.cpp
+
+1. Guess the possible range of the result, choose the right int type.
+
+2. There are no unified standard in different compiler for long long, Maybe `__int64`, maybe` __int64_t`, but use long long.
