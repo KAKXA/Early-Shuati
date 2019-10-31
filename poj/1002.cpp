f1ca9fe39ab59f9948ac4c3af21d37d5a5cb6e29
zgk <runweige@gmail.com>
2019-10-31 22:48:23 +0800

    poj

---
+++ poj/1002.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<map>
+#include<cctype>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	char dic[] = "2223334445556667077888999";
+	map<string,int> m;
+	while(n--){
+		string t;
+		string ta = "";
+		cin >> t;
+		for(char i: t){
+			if(i >= '0' && i <= '9'){
+				ta = ta + i;
+			}
+			else if(i >= 'A' && i <= 'Z'){
+				ta = ta + (dic[i-'A']);
+			}
+		}
+		if(m.count(ta)){
+			m[ta]++;
+		}
+		else{
+			m.insert(pair<string,int>(ta,1));
+		}
+	}
+	for(pair<string,int> i : m){
+		if(i.second > 1){
+			string t = i.first;
+			cout << t.substr(0,3);
+			cout << '-';
+			cout << t.substr(3,4);
+			cout << " " << i.second;
+			cout << endl;
+		}
+	}
+}
