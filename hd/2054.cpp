dec40b648f9bcee13d118b4dfa947e78dde17ae6
ns <notanemail@notanemail.notanemail>
2019-10-31 09:23:27 +0800

    hd

--- hd/2054.cpp
+++ hd/2054.cpp
@@ -3,22 +3,15 @@ using namespace std;
 int main(){
 	string a, b;
 	while(cin >> a >> b){
-		if(a[0] == '-' && b[0] == '-' || a[0] == '+' && b[0] == '+'){
-			a = a.substr(1,a.size() - 1);
-			b = b.substr(1,b.size() - 1);
-		}
-		while(a[0] == '0'){
-			a = a.substr(1,a.size() - 1);
-		}
-		while(b[0] == '0'){
-			b = b.substr(1,b.size() - 1);
-		}
 		for(char i: a){
 			if(i == '.'){
 				while(*a.rbegin() == '0'){
 					a = a.substr(0,a.size() - 1);
 				}
 			}
+		}
+		if(*a.rbegin() == '.'){
+			a = a.substr(0,a.size() - 1);
 		}
 		for(char i: b){
 			if(i == '.'){
@@ -27,7 +20,9 @@ int main(){
 				}
 			}
 		}
-
+		if(*b.rbegin() == '.'){
+			b = b.substr(0,b.size() - 1);
+		}
 		if(a == b){
 			cout << "YES" << endl;
 		}
